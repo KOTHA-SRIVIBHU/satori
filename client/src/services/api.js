a5fe1b9bf49442b8082e4548f16d25fc691cfd94
Vibhu <kothasrivibhu.23bds1057@gmail.com>
2026-05-18 17:25:16 +1100

    ver 1.11 deploy:frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Our Node.js Gateway URL
+  baseURL: 'https://satori-vbj0.onrender.com/api', // Our Node.js Gateway URL
 });
 
 // Add a request interceptor to include the JWT token
